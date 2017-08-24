d4a0129ebafc5d07cf8558f462b8056a77e011a5
Julia Castro <castro.morales.julia@gmail.com>
2017-08-24 15:59:36 -0400

    update test helper - bug

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,11 +2,11 @@ import jsdom from 'jsdom';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
-const win = doc.defaultView;
+const doc = new jsdom.JSDOM('<!DOCTYPE html><html><body></body></html>');
+const win = doc.window;
 
-global.document = doc;
 global.window = win;
+global.document = window.document;
 
 Object.keys(window).forEach((key) => {
   if (!(key in global)) {
