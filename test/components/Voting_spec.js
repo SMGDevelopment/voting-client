e8365ac7f2c915b11c0e9d1d09fa5d9b709ce952
Julia Castro <castro.morales.julia@gmail.com>
2017-08-25 16:33:36 -0400

    ADDED WRAPPER FOR STATELESS COMPONENENTS also test that the presence of hasVoted props should cause the voting buttons to become disabled

--- test/components/Voting_spec.js
+++ test/components/Voting_spec.js
@@ -8,10 +8,22 @@ import {
 import Voting from '../../src/components/Voting'
 import {expect} from 'chai'
 
+//need to use this wrapper because renderIntoDocument cannot be used with STATELESS COMPONENTS
+//google shallow rendering
+var Wrapper = React.createClass({
+  render: function() {
+    return (
+      <div>{this.props.children}</div>
+    )
+  }
+})
+
 describe('Voting', () => {
   it('renders a pair of buttons', () => {
     const component = renderIntoDocument(
-      <Voting pair={["Trainspotting", "28 Days Later"]} />
+      <Wrapper>
+        <Voting pair={["Trainspotting", "28 Days Later"]} />
+      </Wrapper>
     )
     const buttons = scryRenderedDOMComponentsWithTag(component, 'button')
 
@@ -25,11 +37,27 @@ describe('Voting', () => {
     const vote = (entry) => votedWith = entry
 
     const component = renderIntoDocument(
-      <Voting pair={["Trainspotting", "28 Days Later"]} vote={vote} />
+      <Wrapper>
+        <Voting pair={["Trainspotting", "28 Days Later"]} vote={vote} />
+      </Wrapper>
     )
     const buttons = scryRenderedDOMComponentsWithTag(component, 'button')
     Simulate.click(buttons[0])
 
     expect(votedWith).to.equal("Trainspotting")
+  })
+
+  it('disables buttons when user has voted', () => {
+    const component = renderIntoDocument(
+      <Wrapper>
+        <Voting pair={['Trainspotting', '28 Days Later']}
+                hasVoted="Trainspotting" />
+      </Wrapper>
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button')
+
+    expect(buttons.length).to.equal(2)
+    expect(buttons[0].hasAttribute('disabled')).to.equal(true)
+    expect(buttons[1].hasAttribute('disabled')).to.equal(true)
   })
 })
