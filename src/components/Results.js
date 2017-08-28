2fc27f6a5959d3ff82675706ee25544cb72ca1ca
Julia Castro <castro.morales.julia@gmail.com>
2017-08-28 15:04:12 -0400

    reuse winner component if there is a winner

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Winner from './Winner'
 
 export default class Results extends React.PureComponent {
   getPair() {
@@ -14,6 +15,8 @@ export default class Results extends React.PureComponent {
 
   render() {
     return (
+      this.props.winner ?
+      <Winner ref='winner' winner={this.props.winner} /> :
       <div className="results">
         <div className="tally">
           {this.getPair().map(entry =>
