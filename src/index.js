bc4c1adde3ddd19479836c2831f87f415b42f48a
Julia Castro <castro.morales.julia@gmail.com>
2017-08-30 11:07:12 -0400

    connect to socket.io server, connect to port 8090

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
+import io from 'socket.io-client'
 import reducer from './reducer'
 import App from './components/App'
 import {VotingContainer} from './components/Voting'
@@ -17,7 +18,9 @@ store.dispatch({
       tally: {Sunshine: 2}
     }
   }
-})
+});
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`)
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
